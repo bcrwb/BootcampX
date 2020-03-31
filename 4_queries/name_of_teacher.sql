6ef24ba008855a71aa323833217a15437a571db0
Brent <brentcarey1@outlook.com>
2020-03-31 21:44:29 +0000

    Finished 4_queries

---
+++ 4_queries/name_of_teacher.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN students ON assistance_requests.student_id = students.id
+JOIN cohorts ON cohorts.id = students.cohort_id
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teacher
